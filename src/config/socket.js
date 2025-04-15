2dc3a882180f29d5bfd46061ee52ebf8f18e033d
Quan Ngo <ngotrungquan1412@gmail.com>
2025-04-15 14:49:32 +0700

    FixSocket

--- src/config/socket.js
+++ src/config/socket.js
@@ -2,7 +2,7 @@
 const { Server } = require("socket.io");
 
 let io; // Biến để export và dùng ở nơi khác nếu cần
-
+let users = {};
 function initSocket(server) {
   io = new Server(server, {
     cors: {
@@ -12,16 +12,39 @@ function initSocket(server) {
   });
 
   io.on("connection", (socket) => {
+    console.log("connection");
     console.log("User connected: " + socket.id);
+    console.log("Current users:", users);
 
-    // Nhận và phát tin nhắn
-    socket.on("send_message", (data) => {
-      console.log("Received message:", data);
-      io.emit("receive_message", data);
+    socket.on("join", (userId) => {
+      if (!userId) return;
+      users[userId] = socket.id;
+      socket.userId = userId;
+      console.log("join");
+      console.log("User joined:", userId);
+      console.log("Current users:", users);
+    });
+
+    socket.on("sendMessage", async (data) => {
+      console.log("Received sendMessage event with data:", data);
+      const { senderId, reciveId } = data;
+      if (users[senderId]) {
+        io.to(users[senderId]).emit("receiveMessage", { ...data, self: true });
+      }
+
+      if (users[reciveId]) {
+        io.to(users[reciveId]).emit("receiveMessage", data);
+      } else {
+        console.log("Receiver not connected:", reciveId);
+      }
     });
 
     socket.on("disconnect", () => {
-      console.log("User disconnected: " + socket.id);
+      if (socket.userId) {
+        console.log("User disconnected:", socket.userId);
+        delete users[socket.userId];
+        console.log("Current users after disconnect:", users);
+      }
     });
   });
 }
